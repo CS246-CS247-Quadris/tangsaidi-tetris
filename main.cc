76a8e97e8b0f920c0a8b4399e6da15f39e3402f5
h25qi <h25qi@uwaterloo.ca>
2019-07-05 18:34:24 -0400

    Add argument map and parser

--- main.cc
+++ main.cc
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
-int main() {
+enum ArgumentType {
+	ARG_TEXT,
+	ARG_SEED,
+	ARG_SCRIPT,
+	ARG_LEVEL
+};
+
+const map<string, ArgumentType> mode = {
+	{"-text", ARG_TEXT},
+	{"-seed", ARG_SEED},
+	{"-scriptfile", ARG_SCRIPT},
+	{"-startlevel", ARG_LEVEL}
+};
+
+int main(int argc, char *argv[]) {
+	string cmd;
+	
+	for(int opt=1; opt<argc; opt++) {
+		cmd = argv[opt];
+		
+		switch(mode[cmd]) {
+			case ARG_TEXT:
+				// TODO: set to text mode
+				break;
+			case ARG_SEED:
+				// TODO: read one more, set rand seed
+				if(opt != argc-1) {
+					
+				}
+				// error otherwise: missing argument
+				break;
+			case ARG_SCRIPT:
+				// TODO: read one more, set istream from cin to ifstream
+				if(opt != argc-1) {
+					
+				}
+				// error otherwise: missing argument
+				break;
+			case ARG_LEVEL:
+				// TODO: read one more, set level
+				if(opt != argc-1) {
+					
+				}
+				// error otherwise: missing argument
+				break;
+			default:
+				// error: unrecognized command
+				break;
+		}
+	}
+	
 	return 0;
 }
