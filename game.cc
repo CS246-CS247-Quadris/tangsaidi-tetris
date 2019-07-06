3a0aa900ffdd530751641c9923842c8fb330defc
h25qi <h25qi@uwaterloo.ca>
2019-07-06 16:57:10 -0400

    Update Game constructor

--- game.cc
+++ game.cc
@@ -1,7 +1,7 @@
 #include "game.h"
 using namespace std;
 
-Game::Game() : game{make_unique<Board>()}{}
+Game::Game(bool isTextMode, int level, istream& is): in{is}, game{make_unique<Board>(level)}{}
 
 void Game::parseCommand(const std::string& cmd) {
 	
